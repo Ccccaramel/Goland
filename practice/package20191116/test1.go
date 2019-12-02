78bc0e0ba823bd4b67778cc1e725164f0e1e57e3
bunny <444543565@qq.com>
2019-12-02 19:59:45 +0800

    日常更新

--- practice/package20191116/test1.go
+++ practice/package20191116/test1.go
@@ -1,6 +1,26 @@
-package main
-import "fmt"
+package main // 定义包名
 
-func main(){
-	fmt.Println("Hello!")
+import "fmt" // 告诉 Go 编译器这个程序需要用 fmt 包(的函数,或其它元素), fmt 包实现了格式化 IO(输入/输出) 的函数
+
+func main() { // 入口函数.若有 init() 函数则会先执行 init() 函数
+	fmt.Println("Hello!") // fmt.Print(...) 可以将字符串输出到控制台
+	fmt.Print("Hi!" + "Golang!")
 }
+
+/*
+标识符:
+	由字母、数字、下划线组成,不可以以数字开头
+
+关键字:
+	break       default     func        interface   select
+	case        defer       go          map         struct
+	chan        else        goto        package     switch
+	const       fallthrough if          range       type
+	continue    for         import      return      var
+
+预定义字符:
+	append      bool        byte        cap         close       complex     complex64   complex128  uint16
+	copy        false       float32     float64     imag        int         int8        int16       uint32
+	int32       int64       iota        len         make        new         nil         panic       uint64
+	print       println     real        recover     string      true        uint        uint8       uintptr
+*/
--- practice/package20191116/test2.go
+++ practice/package20191116/test2.go
@@ -1,6 +1,12 @@
 package main
+
 import "fmt"
 
-func main(){
-	fmt.Print("Hi!")
+func main() {
+	fmt.Print("Hi!!")
 }
+
+/*
+标识符:由字母、数字、下划线组成,不可以以数字开头
+
+*/
