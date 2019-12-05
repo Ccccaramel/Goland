b43b5dcd2e9280ee80043c47610dbb8c51d5ede2
bunny <444543565@qq.com>
2019-12-05 12:02:14 +0800

    日常更新

--- practice/package20191116/test7.go
+++ practice/package20191116/test7.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type Phone interface {
+	call()
+}
+type IPhone struct {
+}
+type NokiaPhone struct {
+}
+
+func (nokiaPhone NokiaPhone) call() {
+	fmt.Println("I am Nokia,I can call you!")
+}
+func (iPhone IPhone) call() {
+	fmt.Println("I am iPhone,I can call you!")
+}
 func main() {
 	num1 := []int{2, 3, 4}
 	for k, v := range num1 {
@@ -29,12 +43,24 @@ func main() {
 	if ok {
 		fmt.Printf("American 的首都是 ", cou)
 	} else {
-		fmt.Printf("American is notyhingness")
+		fmt.Printf("American is notyhingness\n")
 	}
+
+	delete(map1, "France")
+	for cou := range map1 {
+		fmt.Println(cou, " 首都是 ", map1[cou])
+	}
+
+	var phone Phone
+	phone = new(NokiaPhone)
+	phone.call()
+
+	phone = new(IPhone)
+	phone.call()
 }
 
 /*
-range:
+range():
 	go 语言中 range 关键字用于 for 循环中迭代数组(array)、切片(slice)、通道(channel)、集合(map)的元素
 	在数组和切片中它返回元素的索引和索引对应的值,在集合中返回 key-value 对
 
@@ -49,4 +75,15 @@ map:
 			var map_name map[key_data_type]value_data_type  //默认 map 是 nil
 			map_name := make(map[key_data_type]value_data_type)  //使用 make 函数
 
+delete():
+	用于删除集合的元素,参数为 map 和其对应的 key
+
+递归:
+	pass
+
+类型转换:
+	type_name(expression)
+
+接口:
+	接口也属于一种数据类型,它把所有具有共性的方法定义在一起,任何其它类型只要实现这些方法就是实现了这个接口
 */
